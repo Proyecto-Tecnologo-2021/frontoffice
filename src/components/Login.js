72807ed3bb20930fcbb80a0b69984eaeadc70515
Gonzalo <gonzalo.santa.maria@fing.edu.uy>
2021-10-18 20:33:35 -0300

    Login - segunda version

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, {Component, useState} from "react";
 import {Redirect} from "react-router";
-// import "../assets/css/Login.css";
+import "../assets/css/Login.css";
 import {
     Badge,
     Button,
@@ -11,6 +11,7 @@ import {
     Container,
     Row,
     Col,
+    ListGroup, ButtonGroup
 } from "react-bootstrap";
 
 const Login = () => {
@@ -36,77 +37,114 @@ const Login = () => {
     }
 
     return (
-            <Container style={{ position: `relative`}}>
-                <Row className="justify-content-md-center " md="2" style={{top: `50%`}}>
-                    <Col>
-                        <Card>
-                            <Card.Header>
-                                <Card.Title as="h4">Bienvenido a Appetit</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                                <Form action='/home'>
-                                    <Row>
-                                        <Col className="pl-1" >
-                                            <Form.Group>
-                                                <label htmlFor="exampleInputEmail1">
-                                                    Email
-                                                </label>
-                                                <Form.Control
-                                                    placeholder="Email"
-                                                    type="email"
-                                                ></Form.Control>
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
+        <div id="divLogin">
+            <Container>
+                <Row>
+                    <Col className="d-flex align-items-center">
+                        <Card className="rounded-3 shadow-lg" id="cardLogin" >
+                            <ListGroup variant="flush" >
+                                <ListGroup.Item >
+                                    <div>
+                                        <img
+                                            // width="104"
+                                            // height="85"
+                                            width="125"
+                                            height="125"
+                                            src={require("assets/img/logoAppetit.png").default}
+                                            alt="..."
+                                        />
+                                    </div>
+                                    <Card.Header>
+                                        <Card.Title as="h4">Ingreso de Usuario</Card.Title>
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <Form action='/home'>
+                                            <Row>
+                                                <Col className="pl-1">
+                                                    <Form.Group>
+                                                        <label className="d-flex justify-content-start">
+                                                            Email
+                                                        </label>
+                                                        <Form.Control
+                                                            placeholder="Email"
+                                                            type="email"></Form.Control>
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
 
-                                    <Row>
-                                        <Col className="pr-1" >
-                                            <Form.Group>
-                                                <label>Contraseña</label>
-                                                <Form.Control
-                                                    // defaultValue="Mike"
-                                                    placeholder="Contraseña..."
-                                                    type="password"
-                                                ></Form.Control>
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <br/>
+                                            <Row>
+                                                <Col className="pr-1">
+                                                    <Form.Group>
+                                                        <label
+                                                            className="d-flex justify-content-start mt-1">Contraseña</label>
+                                                        <Form.Control
+                                                            // defaultValue="Mike"
+                                                            placeholder="Contraseña..."
+                                                            type="password"
+                                                        ></Form.Control>
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+                                            <br/>
 
-                                    <Row>
-                                        <div className="custom-control custom-checkbox">
-                                            <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-                                            &nbsp;
-                                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                                        </div>
-                                    </Row>
-                                    <br/>
+                                            <Row>
+                                                <Col>
+                                                    <div className="d-flex justify-content-start ms-2">
+                                                        <input type="checkbox" className="custom-control-input"
+                                                               id="customCheck1"/>
+                                                        &nbsp;
+                                                        <label className="custom-control-label"
+                                                               htmlFor="customCheck1">Recuérdame</label>
+                                                    </div>
+                                                </Col>
+                                                <Col>
+                                                    <label className="d-flex justify-content-end">
+                                                        ¿Olvidó su contraseña? &nbsp;<a href="#">Recupérala</a>
+                                                    </label>
+                                                </Col>
+                                            </Row>
+                                            <br/>
 
-                                    <Row>
+                                            <Row>
+                                                <Button
+                                                    className="shadow-sm btn-fill pull-right"
+                                                    type="submit"
+                                                    variant="warning">
+                                                    Ingresar
+                                                    &nbsp;
+                                                    <i className="fas fa-sign-in-alt"></i>
+                                                </Button>
+                                            </Row>
+                                            <br/>
+
+                                            <Row>
+                                                <Col>
+                                                    <label>
+                                                        ¿Eres nuevo? &nbsp;<a href="#">Crea una cuenta</a>
+                                                    </label>
+                                                </Col>
+                                            </Row>
+                                            <div className="clearfix"></div>
+                                        </Form>
+                                    </Card.Body>
+                                </ListGroup.Item>
+                                <ListGroup.Item className="d-flex justify-content-end">
+                                    <ButtonGroup>
                                         <Button
                                             className="btn-fill pull-right"
-                                            type="submit"
-                                            variant="info"
-                                        >
-                                            Ingresar
+                                            variant="danger">
+                                            <i className="fab fa-google"></i>
+                                            &nbsp;|
+                                            Ingresar con Google
                                         </Button>
-                                    </Row>
-                                    <br/>
-
-                                    <Row>
-                                        <p className="forgot-password text-right">
-                                            Forgot <a href="#">password?</a>
-                                        </p>
-                                    </Row>
-                                    <br/>
-
-                                    <div className="clearfix"></div>
-                                </Form>
-                            </Card.Body>
+                                    </ButtonGroup>
+                                </ListGroup.Item>
+                            </ListGroup>
                         </Card>
                     </Col>
                 </Row>
             </Container>
+        </div>
 
         // {/*</Form>*/}
         // <div className="Login">
