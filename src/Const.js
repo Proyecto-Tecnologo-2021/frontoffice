def8e3e9779e66aece987acc99ee665413ff13ae
Gonzalo <gonzalo.santa.maria@fing.edu.uy>
2021-11-05 03:07:47 -0300

    - Se agrega constante de producción (booleano: true = producción) - Maqueta de constantes para redirigir a Back.

--- src/Const.js
+++ src/Const.js
@@ -1,5 +1,31 @@
+const prod = false;
 export const localDevelopment = false;
-export const URL_Services = "http://localhost:8080/appettit-web/rest"; //LOCAL
+
+export const URL_Services = () => {
+    if (!prod){ //LOCAL
+        return "http://localhost:8080/appettit-web/rest";
+    }else{ //ES PROD
+        return "[LA DIRECCIÓN DE PRODUCCIÓN]";
+    }
+}
+
+export const URL_AltaRestaurante = () => {
+    if (!prod){ //LOCAL
+        return "[URL LOCAL DE ALTA DE RESTAURANTE]";
+    }else{ //ES PROD
+        return "[PROD]";
+    }
+}
+
+export const URL_IndexBackoffice = () => {
+    if (!prod){ //LOCAL
+        return "[URL LOCAL DE LA INDEX DE BACKOFFICE]"; //ALGO ASÍ: http://localhost:8080/appettit-web/restaurante/index.xhtml
+    }else{ //ES PROD
+        return "[PROD]";
+    }
+}
+
+//Utils de pedidos
 export const paypalClientId = 'AX1tidD2Nabve-nXf9dR6UqFx6LtKK4-FJZ9QCzXJgoM8pjYrDNCCPF6KzOLgDDrGZSpaDA2psnjfP5l'
 export const dollarVal = 44.5
 
@@ -16,8 +42,3 @@ export const Dirreccion_Eliminar = "/usuarios/eliminarDireccion/" // PUT (AGREGA
 
 // SERVICIOS DE MENU
 export const menuListar = '/menu' // GET
-
-
-//PROD
-// export const URL_Services = "";
-//export const menuListar = '/menu' //GET
